9823ded81c04f21f1f7dd952e1344f8a4c329001
yogita patil <yogitas372@gmail.com>
2025-04-14 11:51:42 +0530

    ui modifications

---
+++ lib/screens/web view/web_view_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+import '../../constants/app_colors.dart';
+import '../../constants/app_textstyles.dart';
+
+class WebViewScreen extends StatefulWidget {
+  final String url;
+  final String title;
+
+  const WebViewScreen({
+    super.key,
+    required this.url,
+    required this.title,
+  });
+
+  @override
+  State<WebViewScreen> createState() => _WebViewScreenState();
+}
+
+class _WebViewScreenState extends State<WebViewScreen> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // Show loading progress
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse(widget.url));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(widget.title, style: semiBoldTextStyle(fontSize: dimen16, color: Colors.black),),
+      ),
+      backgroundColor: AppColors.ghostWhite,
+      body: WebViewWidget(controller: _controller),
+    );
+  }
+}
