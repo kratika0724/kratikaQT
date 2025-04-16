50e4c3cd0be7a16ae8cf9502ff375bb6cc7ceea9
yogita patil <yogitas372@gmail.com>
2025-04-16 10:08:59 +0530

    widget addition: AppThemeButton

---
+++ lib/widgets/app_theme_button.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import '../../constants/app_colors.dart';
+import '../../constants/app_textstyles.dart';
+
+class AppThemeButton extends StatelessWidget {
+  final String label;
+  final VoidCallback onPressed;
+  final double fontSize;
+  final EdgeInsetsGeometry padding;
+  final Color backgroundColor;
+
+  const AppThemeButton({
+    Key? key,
+    required this.label,
+    required this.onPressed,
+    this.fontSize = 14,
+    this.padding = const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+    this.backgroundColor = AppColors.secondary,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        gradient:  LinearGradient(
+          colors: [AppColors.primary, AppColors.primary.withOpacity(0.6),],
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.transparent,
+          shadowColor: Colors.transparent,
+          padding: padding,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        ),
+        child: Text(
+          label,
+          overflow: TextOverflow.ellipsis,
+          maxLines: 1,
+          style: boldTextStyle(fontSize: fontSize, color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
